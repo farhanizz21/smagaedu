35b966c701e9bd11b3c0603b960539284ed2f18f
dianitaamelia <dianitaamelia2@gmail.com>
2026-07-25 19:22:25 +0700

    smagaedu changes

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/smartedu/';
+$config['base_url'] = 'http://localhost/smagaedu/';
 // $config['base_url'] = 'https://3b1ad8f77461.ngrok-free.app/smartedu/';
 /*
 |--------------------------------------------------------------------------
